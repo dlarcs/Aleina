6ca53f6fbbf98ca97c96fadd1d6333b8e19044aa
Laura  Rozo <admin@promoflow.net>
2026-06-26 10:36:19 -0500

    “…”

--- controller/register.php
+++ controller/register.php
@@ -4,8 +4,8 @@
    public function  handleRegister(){
      $input = file_get_contents('php://input');
      $data = json_decode($input,true);
-alert("holaaa");
      echo json_encode("lo hicimos");exit;
+     
      echo json_encode($data['name']."  ".$data["email"]."  ".$data['password']);
 
      switch ($data['action']) {
